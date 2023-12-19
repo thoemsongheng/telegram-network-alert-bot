e2471554cd439e95d939ea3ec0e524a4b2c90e38
thoemsongheng <songhengthoem99@gmail.com>
2023-12-19 15:32:40 +0700

    initial commit

---
+++ data/hosts.ts
@@ -0,0 +1,86 @@
+// NOTE
+// ip  : is adress of network
+// isp : internet service provider name
+// sid : service id of network (optional)
+// cid : customer id of network (optional)
+// loc : location or network equipment address
+
+export const hosts = [
+  {
+    ip: "96.9.79.137",
+    isp: "SI NET",
+    sid: "15472",
+    cid: "",
+    loc: "Little Server Room BKK",
+  },
+  {
+    ip: "136.228.159.82",
+    isp: "SI NET",
+    sid: "22579",
+    cid: "",
+    loc: "Delivery hall BKK",
+  },
+  {
+    ip: "96.9.77.39",
+    isp: "SI NET",
+    sid: "22137",
+    cid: "",
+    loc: "Mediaload Server room BKK",
+  },
+  {
+    ip: "103.14.251.20",
+    isp: "SI NET",
+    sid: "25340",
+    cid: "",
+    loc: "HQ, Server room ChoukVa",
+  },
+  {
+    ip: "136.228.128.21",
+    isp: "SI NET",
+    sid: "19415",
+    cid: "",
+    loc: "TK, Techdesk Sensok",
+  },
+  {
+    ip: "96.9.74.13",
+    isp: "SI NET",
+    sid: "24698",
+    cid: "",
+    loc: "CC, Frontdesk Chom Chav",
+  },
+  {
+    ip: "119.82.250.154",
+    isp: "EZECOM",
+    sid: "",
+    cid: "33179",
+    loc: "PH, Tv panel Peng Hout",
+  },
+  {
+    ip: "96.9.74.169",
+    isp: "SI NET",
+    sid: "24933",
+    cid: "",
+    loc: "CKD, Tv panel Chamka Doung",
+  },
+  {
+    ip: "96.9.74.125",
+    isp: "SI NET",
+    sid: "25340",
+    cid: "",
+    loc: "RSK, Dwarf floor Rouesey Keo",
+  },
+  {
+    ip: "96.9.88.155",
+    isp: "SI NET",
+    sid: "",
+    cid: "",
+    loc: "Rana, Rack main floor Toul Kork",
+  },
+  {
+    ip: "103.118.46.110",
+    isp: "TODAY",
+    sid: "",
+    cid: "",
+    loc: "Rana, ack main floor Toul Kork",
+  },
+];
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+require("dotenv").config();
+import { Telegraf } from "telegraf";
+import { CronJob } from "cron";
+import ping from "ping";
+import moment from "moment-timezone";
+import { message } from "telegraf/filters";
+import { hosts } from "../data/hosts";
+
+const TOKEN = process.env.TOKEN;
+const GROUP_ID = "-4012462704";
+const bot = new Telegraf(String(TOKEN));
+
+const format = "hh:mm:ss";
+const currentTime = moment().tz("Asia/Phnom_Penh");
+const start = moment("05:00:00", format);
+const end = moment("20:00:00", format);
+
+let alive: { host: string; live: boolean; time: moment.Moment }[] = [];
+
+const job = new CronJob(
+  "*/5 * * * * *",
+  async () => {
+    for (let host of hosts) {
+      let res = await ping.promise.probe(host.ip);
+      if (!currentTime.isBetween(start, end)) return;
+      if (res?.alive) {
+        const found = alive.find((x) => x.host === host.ip);
+        if (found) {
+          if (found.live === false) {
+            found.live = true;
+            const message = `● Alert: RECOVERY ✅\n- Type: UP\n- name: Little Digital Main Link\n- IP: ${host.ip}\n- SID: ${host.sid}\n- CID: ${host.cid}\n- Address: ${host.loc}\n${res.output}`;
+            // bot.telegram.sendMessage(GROUP_ID, message);
+            console.log(message);
+          }
+        } else {
+          alive.push({ host: host.ip, live: true, time: currentTime });
+        }
+      }
+      if (!res?.alive) {
+        const found = alive.find((x) => x.host === host.ip);
+        if (found) {
+          if (found.live === true) {
+            found.live = false;
+            const message = `● Alert: PROBLEM ❌\n- Type: DOWN\n- name: Little Digital Main Link\n- IP: ${host.ip}\n- SID: ${host.sid}\n- CID: ${host.cid}\n- Address: ${host.loc}\n${res.output}`;
+            // bot.telegram.sendMessage(GROUP_ID, message);
+            console.log(message);
+          }
+        } else {
+          alive.push({ host: host.ip, live: false, time: currentTime });
+        }
+      }
+    }
+  },
+  null,
+  true,
+  "Asia/Phnom_Penh"
+);
+
+job.start();
+
+bot.launch();
